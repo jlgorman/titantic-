46da8bc15daef139a262bd5fbe73ff4107617328
JGorman <jameslouisgorman@gmail.com>
2018-05-21 15:30:55 -0500

    removed unpredictive variables

--- titanic_analysis.r
+++ titanic_analysis.r
@@ -2,9 +2,10 @@
 library (caret)
 library(ggplot2)
 library(corrplot)
+library(e1071)
 #load data
-train <- read.csv("D:/Data_Science/Data/titanic/train.csv",StringsAsFactors=FALSE)
-test <- read.csv("D:/Data_Science/Data/titanic/test.csv", StringsAsFactors=FALSE)
+train <- read.csv("D:/Data_Science/Data/titanic/train.csv" , header = TRUE)
+test <- read.csv("D:/Data_Science/Data/titanic/test.csv", header = TRUE)
 
 #quick summary of the data
 dim(train)
@@ -19,3 +20,26 @@ summary(train)
 #convert survived to factor
 train[,2] <- as.factor((train[,2]))
 
+#drop unneccesary variables in effort to increase performance
+train <- train[,c(-1, -4, -9, -11, -12)]
+
+# 10-fold cross validation with 3 repeats
+trainControl <- trainControl(method="repeatedcv", number=10, repeats=3)
+metric <- "Accuracy"
+
+###compare several algorithms
+##Linear Algorithms: 
+#Logistic Regression
+set.seed(5)
+glm.fit <- train(Survived~., data=train, method="glm", metric=metric, trControl=trainControl, na.action = na.pass)
+
+
+#Linear Discriminate Analysis 
+set.seed(5)
+lda.fit <- train(Survived~., data=train, method="lda", metric=metric, trControl=trainControl, na.action = na.pass)
+
+
+
+#and Regularized Logistic Regression (GLMNET).
+#Non-Linear Algorithms: k-Nearest Neighbors (KNN), Classification and Regression Trees (CART),
+#Naive Bayes (NB) and Support Vector Machines with Radial Basis Functions (SVM).
