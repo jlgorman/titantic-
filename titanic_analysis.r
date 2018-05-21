e112443a6f9f6aeaa03d2fdb3b96a033ad777578
JGorman <jameslouisgorman@gmail.com>
2018-05-21 11:24:34 -0500

    import packages, quick summary of data

--- titanic_analysis.r
+++ titanic_analysis.r
@@ -1,3 +1,16 @@
+#load packages
+library (caret)
+library(ggplot2)
+library(corrplot)
 #load data
-train <- read.csv("D:/Data_Science/Data/titanic/train.csv")
-test <- read.csv("D:/Data_Science/Data/titanic/test.csv")
+train <- read.csv("D:/Data_Science/Data/titanic/train.csv",StringsAsFactors=FALSE)
+test <- read.csv("D:/Data_Science/Data/titanic/test.csv", StringsAsFactors=FALSE)
+
+#quick summary of the data
+dim(train)
+
+head(train)
+
+sapply(train, class)
+
+summary(train)
